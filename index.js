ffdff65423e1ee4d8562ebbe54d105f577053a43
testwareai <aitestware@gmail.com>
2024-03-13 00:13:23 +0530

    port changed

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 
 // Other setup code
 
-app.listen(11434, "0.0.0.0", () => {
+// Status route
+app.get("/status", (req, res) => {
+  res
+    .status(200)
+    .send({ status: "OK", message: "Server is running smoothly." });
+});
+
+app.listen(11435, "0.0.0.0", () => {
   console.log("Server is running on port 11434");
 });
